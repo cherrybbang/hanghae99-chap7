d09b0c4bde37dd9c6d5cc2568954043e89a53947
JiYu <eunjun1018@gmail.com>
2025-05-13 20:08:08 +0900

    msw

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,42 +1,39 @@
-import { rest } from 'msw';
+import { http, HttpResponse } from 'msw';
 
 import { events } from '../__mocks__/response/events.json' assert { type: 'json' };
 import { Event } from '../types';
 
 export const handlers = [
-  rest.get('/api/events', (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ events }));
+  http.get('/api/events', () => {
+    return HttpResponse.json({ events });
   }),
 
-  rest.post('/api/events', async (req, res, ctx) => {
-    const newEvent = (await req.json()) as Event;
+  http.post('/api/events', async ({ request }) => {
+    const newEvent = (await request.json()) as Event;
     newEvent.id = String(events.length + 1);
-    events.push(newEvent);
-    return res(ctx.status(201), ctx.json(newEvent));
+    return HttpResponse.json(newEvent, { status: 201 });
   }),
 
-  rest.put('/api/events/:id', async (req, res, ctx) => {
-    const { id } = req.params;
-    const updatedEvent = (await req.json()) as Event;
+  http.put('/api/events/:id', async ({ params, request }) => {
+    const { id } = params;
+    const updatedEvent = (await request.json()) as Event;
     const index = events.findIndex((event) => event.id === id);
 
     if (index !== -1) {
-      events[index] = { ...events[index], ...updatedEvent };
-      return res(ctx.status(200), ctx.json(events[index]));
+      return HttpResponse.json({ ...events[index], ...updatedEvent });
     }
 
-    return res(ctx.status(404));
+    return new HttpResponse(null, { status: 404 });
   }),
 
-  rest.delete('/api/events/:id', (req, res, ctx) => {
-    const { id } = req.params;
+  http.delete('/api/events/:id', ({ params }) => {
+    const { id } = params;
     const index = events.findIndex((event) => event.id === id);
 
     if (index !== -1) {
-      events.splice(index, 1);
-      return res(ctx.status(204));
+      return new HttpResponse(null, { status: 204 });
     }
 
-    return res(ctx.status(404));
+    return new HttpResponse(null, { status: 404 });
   }),
 ];
