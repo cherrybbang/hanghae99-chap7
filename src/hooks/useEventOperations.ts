0b886e7c1c8b10b04118f62d2d16e862f24cb5c4
JiYu <eunjun1018@gmail.com>
2025-05-13 16:53:25 +0900

    deploy

--- src/hooks/useEventOperations.ts
+++ src/hooks/useEventOperations.ts
@@ -3,13 +3,17 @@ import { useEffect, useState } from 'react';
 
 import { Event, EventForm } from '../types';
 
+const isProduction = import.meta.env.MODE === 'production';
+
 export const useEventOperations = (editing: boolean, onSave?: () => void) => {
   const [events, setEvents] = useState<Event[]>([]);
   const toast = useToast();
 
   const fetchEvents = async () => {
     try {
-      const response = await fetch('https://cherrybbang.github.io/hanghae99-chap7/api/events');
+      const response = await fetch(
+        isProduction ? '/hanghae99-chap7/realEvents.json' : '/api/events'
+      );
       if (!response.ok) {
         throw new Error('Failed to fetch events');
       }
